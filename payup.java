5e4c7bf915c35cea5a94128bc9f671927141be7d
zermad007 <smd.bayo@outlook.com>
2025-04-02 14:54:49 -0400

    Delete payup.java
    delete

--- payup.java
+++
@@ -1,5 +0,0 @@
-git branch branchName   = creates a new branch
-git branch development  = created the development branch 
-  since we created the development branch from the master branch,
-  The master branch is the upstream of the Development branch  and   
-  The Development branch is the downstream of the master branch 
